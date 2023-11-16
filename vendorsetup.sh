4d724734668a7a08ce964e019d1387e7efecac3e
ShukakuZa <yusufzuzu787@gmail.com>
2023-11-16 12:35:57 +0000

    Surya: Added vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/xiaomi-surya/android_vendor_xiaomi_surya.git -b lineage-21 vendor/xiaomi/surya
+
+git clone https://github.com/AOSPA/android_kernel_xiaomi_surya.git -b uvite kernel/xiaomi/surya
