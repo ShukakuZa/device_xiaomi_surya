eab71835fd9b0b1e69b6767f2c9b134107da9a67
ShukakuZa <yusufzuzu787@gmail.com>
2024-08-05 19:56:13 +0000

    Mistify Surya

--- mist_surya.mk
+++ mist_surya.mk
@@ -12,34 +12,32 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/mist/config/common_full_phone.mk)
 
 # RisingOS
-TARGET_CORE_GMS := true
+WITH_GAPPS := true
+MIST_MAINTAINER := TeamMist
+MIST_BUILDTYPE := OFFICIAL
 WITH_GMS := true
 TARGET_ENABLE_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := false
 TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_INCLUDE_LAWNCHAIR := true
 
 # Device Manufacture
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := mist_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 USE_GMS := true
-RISING_MAINTAINER=Skyy丨アラタ
 
 BUILD_FINGERPRINT := POCO/surya_global/surya:12/RKQ1.211019.001/V14.0.2.0.SJGMIXM:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="surya_global-user 12 RKQ1.211019.001 V14.0.2.0.SJGMIXM release-keys"
     
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RISING_CHIPSET="Qualcomm Snapdragon 732G" \
-    RISING_MAINTAINER="Skyy丨アラタ"
-
 PRODUCT_PACKAGES += \
     LatinIMEGooglePrebuilt
