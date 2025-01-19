4512e1907d275bbd3632bd72404bc2f1ec94249d
ShukakuZa <yusufzuzu787@gmail.com>
2025-01-19 20:26:09 +0000

    surya: Mistify Bringup

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/crdroidandroid/android_hardware_xiaomi.git -b 15.0 hardware/xiaomi
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_surya.git -b 15.0 kernel/xiaomi/surya
+git clone https://gitlab.com/crdroidandroid/android_vendor_miuicamera.git -b leica-5.0-surya vendor/xiaomi/miuicamera
+git clone https://github.com/ShukakuZa/vendor_xiaomi_surya.git -b 15 vendor/xiaomi/surya
